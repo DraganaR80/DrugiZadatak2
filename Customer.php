70b47e15ead1aefe38f75e9cade1be408af16a98
Dragana Reljic <reljicdragana@yahoo.com>
2023-01-15 11:21:43 +0100

    metode

--- Customer.php
+++ Customer.php
@@ -20,6 +20,24 @@ class Customer {
     }
 
 
+public function buy (Product $p, $quantity=1){ //racuna se ako kupi da je to 1 proizvod,,iyvrsi kupovinu ako mozes da smanjis za ovoliko
+
+        if ($this->money < $p->getPrice()*$quantity) //cena puta kolicina, da li imamo dovoljno novca da kupimo
+            echo " Not enough money";
+        else {
+            if($p->reduceAmount($quantity)) //ako u prudct vratz TRUE, ulazimo u ovaj if smanjuje s ekolicina novca za br proizvoda koji je kupljen
+            $this->money -= $p->getPrice()* $quantity;
+            else
+                echo " We are out of {$p->getProductName()}s";// u suprotnom nemamo proizvoda na stanju
+        }
+}
+
+
+public function getMoney(){
+
+        return $this->money;
+}
+
 public function __toString()
 {
         return " Customer $this->firstname $this->lastname purchased merchandise for {$this->money}din <br>";
--- index.php
+++ index.php
@@ -7,7 +7,7 @@ require "Product.php";
 $c1 = new Customer(1, "Pera", "Peric", 30, 1500);
 $c2 = new Customer(2, "Ana", "Bojcic",25, 2000);
 $c3 = new Customer(3, "Zarko", "Zec",16, 800);
-$c4 = new Customer(4, "Jelena", "Babic",20, 1000);
+$c4 = new Customer(4, "Jelena", "Babic",70, 1000);
 
 
 $p1 = new Product(1, "Blic", " newspapers", 50, 450);
@@ -20,4 +20,7 @@ $p5 = new Product(5, "Bermet", " wine", 650, 400);
 
 echo $c1;
 echo $p1;
-?>
+$c1->buy($p1, 4);
+echo "<br>" . $p1->getAmount() . "<br>";
+$c1->buy($p3);
+echo $c1->getMoney();
