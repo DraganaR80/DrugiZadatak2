a28738c70571144535566287617a89910847709a
Dragana Reljic <reljicdragana@yahoo.com>
2023-01-15 11:20:19 +0100

    dodate metode

--- Product.php
+++ Product.php
@@ -1,13 +1,14 @@
 <?php
 
-class Product{
+class Product
+{
     private $id;
     private $productName;
     private $productType;
     private $price;
     private $amount;
 
-    public function __construct($id,$productName,$productType,$price,$amount)
+    public function __construct($id, $productName, $productType, $price, $amount)
     {
         $this->id = $id;
         $this->productName = $productName;
@@ -16,17 +17,43 @@ class Product{
         $this->amount = $amount;
     }
 
-    
-public function __toString()
+    public function getPrice()
+    {
+
+        return $this->price;
+    }
+
+public function getProductName()
 {
+        return $this->productName;
+
+}
+
+public function getAmount(){
+
+        return $this->amount;
+}
+    public function reduceAmount($quantity)
+    {
+        if ($this->amount >=$quantity) { //ne moze da izvrsi kupovinu kolicine koja ne postoj, mozemo da kupimo kao je amount >= sa quantity
+            $this->amount-=$quantity;    //oduzima se od pocetne kolicine , ako nije >= ne moze da se kupi
+            return TRUE;// vraca true kad je uspesna kupovina tj odabrali smo kolicinu proizvoda koliko ima na stanj
+        } else {
+
+            return FALSE;// vraca false kada nema na stanju ta kolicina, kupovina nije uspesna
+        }
+    }
+    public function __toString()
+    {
         return "Product $this->productName is $this->productType.Price of this product is {$this->price}din<br>";
-}
-
-
-
-
+    }
 
 }
+
+
+
+
+	
 
 
 
